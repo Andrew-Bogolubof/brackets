6b44fa5668d78aaceddb55eb61f5c7c8e56d889b
Andrew-Bogolubof <a.bogolubof@gmail.com>
2019-02-20 16:36:26 +0300

    Task Branches

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    //Check str for parity
+    if ( str.length % 2 != 0 )
+        return false;
+
+    //compareBraces is the strings for future comparision. Program make it to stings '()' etc.
+    let compareBraces = [];
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        compareBraces[i] = bracketsConfig[i].join('');
+    }
+
+    //Main idea is to throw away all inner braces till string will be equal 0
+    //helpStr variable help to understand when to stop the while loop
+    while ( str.length !=0 ) {
+        let helpStr = str;
+        for (let i = 0; i < compareBraces.length; i++) {
+            str = str.split(compareBraces[i]).join('');
+        }
+
+        if ( helpStr == str )
+            break;
+
+    }
+
+    if (!str.length)
+        return true;
+    else
+        return false;
 }
